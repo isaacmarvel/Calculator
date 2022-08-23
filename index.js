fec4db05c4d2c0292fb65cd7b0b9567cd4599c16
Isaac <isaac.marvel.77@gmail.com>
2022-08-23 13:31:47 -0400

    Link up js and html, got event listeners working

--- index.js
+++ index.js
@@ -31,50 +31,171 @@ function operate(operator, num1, num2) {
   }
 }
 let display = "";
-console.log(display);
 //function that populates display when i click number button, storing them to display value.
 
 //need to have them display on apge with event listener
 let displayButton1 = function () {
-  display + 1;
+  console.log(display + 1);
+  return
 };
 let displayButton2 = function () {
-  display + 2;
+  return display + 2;
 };
 let displayButton3 = function () {
-  display + 3;
+  return display + 3;
 };
 let displayButton4 = function () {
-  display + 4;
+  return display + 4;
 };
 let displayButton5 = function () {
-  display + 5;
+  return display + 5;
 };
 let displayButton6 = function () {
-  display + 6;
+  return display + 6;
 };
 let displayButton7 = function () {
-  display + 7;
+  return display + 7;
 };
 let displayButton8 = function () {
-  display + 8;
+  return display + 8;
 };
 let displayButton9 = function () {
-  display + 9;
+  return display + 9;
 };
 let displayButton0 = function () {
-  display + 0;
+  return display + 0;
 };
 
-let firstNumber;
-//function to save display:
-let operatorPressed = function(operatorYouClicked) {
-  firstNumber = display;
-  operator = operatorYouClicked;
+let button1 = document.querySelector("#button1");
+button1.addEventListener("click",()=>{
+  display += 1;
+  document.getElementById("displayField").innerHTML = display;
+});
+
+let button2 = document.querySelector("#button2");
+button2.addEventListener("click",()=>{
+  display += 2;
+  document.getElementById("displayField").innerHTML = display;
+});
+
+let button3 = document.querySelector("#button3");
+button3.addEventListener("click",()=>{
+  display += 3;
+  document.getElementById("displayField").innerHTML = display;
+});
+
+let button4 = document.querySelector("#button4");
+button4.addEventListener("click",()=>{
+  display += 4;
+  document.getElementById("displayField").innerHTML = display;
+});
+
+let button5 = document.querySelector("#button5");
+button5.addEventListener("click",()=>{
+  display += 5;
+  document.getElementById("displayField").innerHTML = display;
+});
+
+let button6 = document.querySelector("#button6");
+button6.addEventListener("click",()=>{
+  display += 6;
+  document.getElementById("displayField").innerHTML = display;
+});
+
+let button7 = document.querySelector("#button7");
+button7.addEventListener("click",()=>{
+  display += 7;
+  document.getElementById("displayField").innerHTML = display;
+});
+
+let button8 = document.querySelector("#button8");
+button8.addEventListener("click",()=>{
+  display += 8;
+  document.getElementById("displayField").innerHTML = display;
+});
+
+let button9 = document.querySelector("#button9");
+button9.addEventListener("click",()=>{
+  display += 9;
+  document.getElementById("displayField").innerHTML = display;
+});
+
+let button0 = document.querySelector("#button0");
+button0.addEventListener("click",()=>{
+  display += 0;
+  document.getElementById("displayField").innerHTML = display;
+});
+
+let firstNumber = 0;
+let operator = "";
+
+//sets firstNumber to display number, sets operator, and resets display
+
+let plusButton = document.querySelector("#plusButton");
+plusButton.addEventListener("click",()=>{
+  firstNumber = Number(display);
+  operator = "plus";
+  display = "";
+  document.getElementById("displayField").innerHTML = display;
+});
+
+let minusButton = document.querySelector("#minusButton");
+minusButton.addEventListener("click",()=>{
+  firstNumber = Number(display);
+  operator = "minus";
+  display = "";
+  document.getElementById("displayField").innerHTML = display;
+});
+
+let multiplyButton = document.querySelector("#multiplyButton");
+multiplyButton.addEventListener("click",()=>{
+  firstNumber = Number(display);
+  operator = "multiply";
+  display = "";
+  document.getElementById("displayField").innerHTML = display;
+});
+
+let divideButton = document.querySelector("#divideButton");
+divideButton.addEventListener("click",()=>{
+  firstNumber = Number(display);
+  operator = "divide";
+  display = "";
+  document.getElementById("displayField").innerHTML = display;
+});
+
+
+
+// let plusPressed = function() {
+//   firstNumber = Number(display);
+//   operator = "plus";
+//   display = "";
+//   document.getElementById("displayField").innerHTML = display;
+
+// }
+
+let minusPressed = function() {
+  firstNumber = Number(display);
+  operator = "minus";
   display = "";
 }
 
+let multiplyPressed = function() {
+  firstNumber = Number(display);
+  operator = "multiply";
+  display = "";
+}
+
+let dividePressed = function() {
+  firstNumber = Number(display);
+  operator = "divide";
+  display = "";
+}
+
+// let equalsPressed = function(operatorYouClicked) {
+//   firstNumber = Number(display);
+//   operator = plus;
+//   display = "";
+// }
 //whne operator is pressed, run function to save display, save operation chosen, and operated on them when = is pressed.
 
 //store first number input into the calc when a user presses an operator, save operation chosen, operate on them when = is pressed.
-// document.getElementById("")
