d2e865ebc6e8a7712a2c33193da465c0851df892
Isaac <isaac.marvel.77@gmail.com>
2022-08-23 11:46:20 -0400

    created event listeners for numbers, working on operators

--- index.js
+++ index.js
@@ -30,3 +30,51 @@ function operate(operator, num1, num2) {
       break;
   }
 }
+let display = "";
+console.log(display);
+//function that populates display when i click number button, storing them to display value.
+
+//need to have them display on apge with event listener
+let displayButton1 = function () {
+  display + 1;
+};
+let displayButton2 = function () {
+  display + 2;
+};
+let displayButton3 = function () {
+  display + 3;
+};
+let displayButton4 = function () {
+  display + 4;
+};
+let displayButton5 = function () {
+  display + 5;
+};
+let displayButton6 = function () {
+  display + 6;
+};
+let displayButton7 = function () {
+  display + 7;
+};
+let displayButton8 = function () {
+  display + 8;
+};
+let displayButton9 = function () {
+  display + 9;
+};
+let displayButton0 = function () {
+  display + 0;
+};
+
+let firstNumber;
+//function to save display:
+let operatorPressed = function(operatorYouClicked) {
+  firstNumber = display;
+  operator = operatorYouClicked;
+  display = "";
+}
+
+//whne operator is pressed, run function to save display, save operation chosen, and operated on them when = is pressed.
+
+//store first number input into the calc when a user presses an operator, save operation chosen, operate on them when = is pressed.
+// document.getElementById("")
