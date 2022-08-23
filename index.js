7bd6ad9860aae0f499efdc36d981ef3593ddc2fd
Isaac <isaac.marvel.77@gmail.com>
2022-08-23 15:56:36 -0400

    working on equalsButton

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ let display = "";
 
 let displayButton1 = function () {
   console.log(display + 1);
-  return
+  return;
 };
 let displayButton2 = function () {
   return display + 2;
@@ -65,119 +65,112 @@ let displayButton0 = function () {
 };
 
 let button1 = document.querySelector("#button1");
-button1.addEventListener("click",()=>{
+button1.addEventListener("click", () => {
   display += 1;
   document.getElementById("displayField").innerHTML = display;
 });
 
 let button2 = document.querySelector("#button2");
-button2.addEventListener("click",()=>{
+button2.addEventListener("click", () => {
   display += 2;
   document.getElementById("displayField").innerHTML = display;
 });
 
 let button3 = document.querySelector("#button3");
-button3.addEventListener("click",()=>{
+button3.addEventListener("click", () => {
   display += 3;
   document.getElementById("displayField").innerHTML = display;
 });
 
 let button4 = document.querySelector("#button4");
-button4.addEventListener("click",()=>{
+button4.addEventListener("click", () => {
   display += 4;
   document.getElementById("displayField").innerHTML = display;
 });
 
 let button5 = document.querySelector("#button5");
-button5.addEventListener("click",()=>{
+button5.addEventListener("click", () => {
   display += 5;
   document.getElementById("displayField").innerHTML = display;
 });
 
 let button6 = document.querySelector("#button6");
-button6.addEventListener("click",()=>{
+button6.addEventListener("click", () => {
   display += 6;
   document.getElementById("displayField").innerHTML = display;
 });
 
 let button7 = document.querySelector("#button7");
-button7.addEventListener("click",()=>{
+button7.addEventListener("click", () => {
   display += 7;
   document.getElementById("displayField").innerHTML = display;
 });
 
 let button8 = document.querySelector("#button8");
-button8.addEventListener("click",()=>{
+button8.addEventListener("click", () => {
   display += 8;
   document.getElementById("displayField").innerHTML = display;
 });
 
 let button9 = document.querySelector("#button9");
-button9.addEventListener("click",()=>{
+button9.addEventListener("click", () => {
   display += 9;
   document.getElementById("displayField").innerHTML = display;
 });
 
 let button0 = document.querySelector("#button0");
-button0.addEventListener("click",()=>{
+button0.addEventListener("click", () => {
   display += 0;
   document.getElementById("displayField").innerHTML = display;
 });
 
-
 let firstNumber = 0;
+let secondNumber = 0;
 let operator = "";
 
 //These four set firstNumber to display number, sets operator, and resets display
 let plusButton = document.querySelector("#plusButton");
-plusButton.addEventListener("click",()=>{
+plusButton.addEventListener("click", () => {
   firstNumber = Number(display);
-  // operator = "plus";
   display = "";
   document.getElementById("displayField").innerHTML = display;
 });
 
 let minusButton = document.querySelector("#minusButton");
-minusButton.addEventListener("click",()=>{
+minusButton.addEventListener("click", () => {
   firstNumber = Number(display);
-  // operator = "minus";
   display = "";
   document.getElementById("displayField").innerHTML = display;
 });
 
 let multiplyButton = document.querySelector("#multiplyButton");
-multiplyButton.addEventListener("click",()=>{
+multiplyButton.addEventListener("click", () => {
   firstNumber = Number(display);
-  // operator = "multiply";
   display = "";
   document.getElementById("displayField").innerHTML = display;
 });
 
 let divideButton = document.querySelector("#divideButton");
-divideButton.addEventListener("click",()=>{
-  if (firstNumber === "") {
+divideButton.addEventListener("click", () => {
+  if (operator === "") {
     firstNumber = Number(display);
-    // operator = "divide";
     display = "";
+    operator = "/";
     document.getElementById("displayField").innerHTML = display;
   } else {
-    //should display the result, not the second number you press!
-    display = operate("/", firstNumber, Number(display));
+    display = operate(operator, firstNumber, Number(display));
     document.getElementById("displayField").innerHTML = display;
   }
 });
 
-//basically this should just be the operate part of what's in the above functions
-// let equalsButton = document.querySelector("#equalsButton");
-// equalsButton.addEventListener("click",()=>{
-//   firstNumber = Number(display);
-//   operator = "divide";
-//   display = "";
-//   document.getElementById("displayField").innerHTML = display;
-// });
-
-
-
+let equalsButton = document.querySelector("#equalsButton");
+equalsButton.addEventListener("click", () => {
+  console.log(firstNumber);
+  console.log(operator);
+  console.log(Number(display));
+  display = operate(operator, firstNumber, Number(display));
+  document.getElementById("displayField").innerHTML = display;
+});
 
 // let equalsPressed = function(operatorYouClicked) {
 //   firstNumber = Number(display);
